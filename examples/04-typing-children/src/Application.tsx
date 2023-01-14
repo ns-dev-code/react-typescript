eca154dc90cd614294f825903357e807ccbe186f
Nitin Singh <nitinsingh@Nitins-MacBook-Pro.local>
2023-01-15 02:14:50 +0530

    Completed Typing children

--- examples/04-typing-children/src/Application.tsx
+++ examples/04-typing-children/src/Application.tsx
@@ -6,31 +6,33 @@
  * React.ReactNode;
  * React.ReactChildren;
  * React.ReactChild[];
+ * Best is to use React.ReactNode
  */
 
- type BoxProps = { children: any };
+type BoxProps = {
+  children: React.ReactNode | React.ReactChild | React.ReactChild[];
+};
 
- const Box = ({ children }: BoxProps) => {
-   return (
-     <section style={{ padding: "1em", border: "5px solid purple" }}>
-       {children}
-     </section>
-   );
- };
- 
- export default function Application() {
-   return (
-     <Box>
-       Just a string.
-       <p>Some HTML that is not nested.</p>
-       <Box>
-         <h2>Another React component with one child.</h2>
-       </Box>
-       <Box>
-         <h2>A nested React component with two children.</h2>
-         <p>The second child.</p>
-       </Box>
-     </Box>
-   );
- }
- 
+const Box = ({ children }: BoxProps) => {
+  return (
+    <section style={{ padding: '1em', border: '5px solid purple' }}>
+      {children}
+    </section>
+  );
+};
+
+export default function Application() {
+  return (
+    <Box>
+      Just a string.
+      <p>Some HTML that is not nested.</p>
+      <Box>
+        <h2>Another React component with one child.</h2>
+      </Box>
+      <Box>
+        <h2>A nested React component with two children.</h2>
+        <p>The second child.</p>
+      </Box>
+    </Box>
+  );
+}
